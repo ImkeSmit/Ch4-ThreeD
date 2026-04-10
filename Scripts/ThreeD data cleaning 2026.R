4e3e0843d08d566db0047022d0aa94be8010699b
ImkeSmit <imke610@gmail.com>
2026-04-10 12:58:08 +0200

    make notes on where the missing turfs are

--- Scripts/ThreeD data cleaning 2026.R
+++ Scripts/ThreeD data cleaning 2026.R
@@ -113,10 +113,12 @@ only_in_metadat <- anti_join(metadat_turfs, entered_turfs, by = "turfID")
 veg_only2 <- veg_only |> 
   mutate(turfID = ifelse(turfID == "24_WN10N_103", "24_WN5N_103", turfID),
          turfID = ifelse(turfID == "29_ WN3C_106", "29_WN3C_106", turfID), 
-         turfID = ifelse(turfID == "85 WN1C 162", "85_WN1C_162", turfID))
+         turfID = ifelse(turfID == "85 WN1C 162", "85_WN1C_162", turfID)) 
+#Now replace th eblockID etc of these turfs!
 
 #The missing ones we have to look for are 129_AN4I_129 and 130_AN4M_130
-
+#130 is on page 36 of the second mid site scan
+#129 is on page 38 of the second mid site scan
 
 
 #There are a few plots with no total cover measurements. Add them from looking at the pictures. 
