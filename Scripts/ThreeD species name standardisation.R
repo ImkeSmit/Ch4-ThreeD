bd7ca10cee3279c3e34b65c77eb68080afb8b744
ImkeSmit <imke610@gmail.com>
2026-04-15 09:36:04 +0200

    build in a condition t only change names if it was collected before a certain date

--- Scripts/ThreeD species name standardisation.R
+++ Scripts/ThreeD species name standardisation.R
@@ -46,11 +46,23 @@ standardise_names <- function(data, #dataframe containing species names that nee
       
       if (TRUE %in% found){ # only runs if the species is a synonym
         new_name <- naming_system[j, which(colnames(naming_system) %in% correct_name)] # finds the true name of the species and saves it
+        record_date <- data[i, which(colnames(data) == "Date")] #get date of
+        
+        if(is.na(condition)){
         
         data[i, which(colnames(data) == data_species_column)] <- new_name
         
         #add column to keep track of which names changed
         data[i, which(colnames(data) == "change_tracker")] <- paste0(old_name, " -> ", new_name)
+        
+        } else {
+          ###Only change the name if condition is met
+          if(record_date < condition) { #only change name if date is before the condition date
+            data[i, which(colnames(data) == data_species_column)] <- new_name
+            #add column to keep track of which names changed
+            data[i, which(colnames(data) == "change_tracker")] <- paste0(old_name, " -> ", new_name)
+          }
+        }
       }
     }
     
