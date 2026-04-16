f6b29461e37fa5ce9d6357880d5ad051b262fb09
ImkeSmit <imke610@gmail.com>
2026-04-16 13:12:25 +0200

    Script to map one turf

--- Scripts/Mapping turfs.R
+++ Scripts/Mapping turfs.R
@@ -3,3 +3,33 @@
 library(remotes)
 remotes::install_github("Between-the-Fjords/turfmapper")
 library(turfmapper)
+library(openxlsx)
+library(tidyverse)
+
+comm25 <- read.xlsx("All_data/clean_data/community_2025.xlsx")
+comm26 <- read.xlsx("All_data/clean_data/community_2026.xlsx")
+
+all_years <- bind_rows(comm25,comm26) |> 
+  pivot_longer(cols = 14:38, names_to = "subturf", values_to = "Presence", 
+               names_transform = list(Subturf = as.integer)) |> 
+  filter(Presence != "0") |> 
+  mutate(subturf = as.numeric(subturf)) |> ##subturf names must be numeric
+  rename(year = Year, 
+         species = Species, 
+         cover = Cover, 
+         site_id = destSiteID,
+         turf_id = turfID)
+
+grid<- make_grid(ncol = 5)
+plot_subturf_grid(grid_long = grid)
+
+all_years |>
+  filter(turf_id == "139_AN8I_139") |> 
+  make_turf_plot(
+    data = _,
+    year = year, species = species, cover = cover, 
+    subturf = subturf,
+    site_id = site_id,
+    turf_id = turf_id,
+    grid_long = grid
+  )
