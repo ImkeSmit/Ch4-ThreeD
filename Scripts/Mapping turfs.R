f8ec0944383521ba0ea3ea6e9a8a3ecd74d2148f
ImkeSmit <imke610@gmail.com>
2026-04-16 13:22:19 +0200

    script to plot many species

--- Scripts/Mapping turfs.R
+++ Scripts/Mapping turfs.R
@@ -13,17 +13,18 @@ all_years <- bind_rows(comm25,comm26) |>
   pivot_longer(cols = 14:38, names_to = "subturf", values_to = "Presence", 
                names_transform = list(Subturf = as.integer)) |> 
   filter(Presence != "0") |> 
-  mutate(subturf = as.numeric(subturf)) |> ##subturf names must be numeric
+  mutate(subturf = as.numeric(subturf)) ##subturf names must be numeric
+  
+grid<- make_grid(ncol = 5)
+plot_subturf_grid(grid_long = grid)
+
+#plot one turf
+all_years |>
   rename(year = Year, 
          species = Species, 
          cover = Cover, 
          site_id = destSiteID,
          turf_id = turfID)
-
-grid<- make_grid(ncol = 5)
-plot_subturf_grid(grid_long = grid)
-
-all_years |>
   filter(turf_id == "139_AN8I_139") |> 
   make_turf_plot(
     data = _,
@@ -33,3 +34,32 @@ all_years |>
     turf_id = turf_id,
     grid_long = grid
   )
+
+
+#plot many turfs
+x <- all_years %>% 
+  filter(
+         grepl("Helichrysum", Species),
+         turfID %in% c("67_AN9M_67"),
+  ) %>%
+  arrange(destSiteID, origPlotID, turfID) %>% 
+  group_by(destSiteID, origPlotID, turfID) %>% 
+  rename(year = Year, 
+         species = Species, 
+         cover = Cover, 
+         site_id = destSiteID,
+         turf_id = turfID) %>%
+  nest() %>% 
+  {map2(
+    .x = .$data, 
+    .y = glue::glue("Site {.$destSiteID}: plot {.$destPlotID}: turf {.$turfID}"),
+    .f = ~make_turf_plot(
+      data = .x, 
+      year = year, 
+      species = species, 
+      cover = cover, 
+      subturf = subturf, 
+      title = glue::glue(.y), 
+      grid_long = grid)
+  )} %>% 
+  walk(print)
