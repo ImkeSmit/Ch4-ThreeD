a79e37a2b3ead848248da11bf026cc49fcc8a8f4
ImkeSmit <imke610@gmail.com>
2026-08-03 11:36:06 +0200

    Create script to create some descriptive stats for WP1 progress report

---
+++ Scripts/Wp 1 progress report.R
@@ -0,0 +1,5 @@
+###Descriptive statistics and generating figures for WP 1 progress report####
+library(openxlsx)
+
+veg25 <- read.xlsx("All_data/clean_data/community_2025.xlsx")
+veg26 <- read.xlsx("All_data/clean_data/community_2026.xlsx")
