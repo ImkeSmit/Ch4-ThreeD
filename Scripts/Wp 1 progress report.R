d84b7d352bc2eebc5c7b5eea45d69c72b25fe3f3
ImkeSmit <imke610@gmail.com>
2026-08-03 11:40:45 +0200

    how many species did we find each year

--- Scripts/Wp 1 progress report.R
+++ Scripts/Wp 1 progress report.R
@@ -1,5 +1,18 @@
 ###Descriptive statistics and generating figures for WP 1 progress report####
 library(openxlsx)
+library(tidyverse)
+library(tidylog)
 
 veg25 <- read.xlsx("All_data/clean_data/community_2025.xlsx")
 veg26 <- read.xlsx("All_data/clean_data/community_2026.xlsx")
+
+veg_all <- veg25 |> 
+  bind_rows(veg_26)
+
+###How many species each year?
+veg_all |> 
+  group_by(Year) |> 
+  distinct(Species) |> 
+  summarise(nsp = n())
+
+###Changes in Species per treatment
